5c531e40a8b79e987edb14c8aca07b1cd34f33e9
Dan <lepedaniel8822@gmail.com>
2020-02-05 17:54:08 -0600

    finished

--- Assign01/IntSet.cpp
+++ Assign01/IntSet.cpp
@@ -1,45 +1,6 @@
-// FILE: IntSet.cpp - header file for IntSet class
-//       Implementation file for the IntStore class
-//       (See IntSet.h for documentation.)
-// INVARIANT for the IntSet class:
-// (1) Distinct int values of the IntSet are stored in a 1-D,
-//     compile-time array whose size is IntSet::MAX_SIZE;
-//     the member variable data references the array.
-// (2) The distinct int value with earliest membership is stored
-//     in data[0], the distinct int value with the 2nd-earliest
-//     membership is stored in data[1], and so on.
-//     Note: No "prior membership" information is tracked; i.e.,
-//           if an int value that was previously a member (but its
-//           earlier membership ended due to removal) becomes a
-//           member again, the timing of its membership (relative
-//           to other existing members) is the same as if that int
-//           value was never a member before.
-//     Note: Re-introduction of an int value that is already an
-//           existing member (such as through the add operation)
-//           has no effect on the "membership timing" of that int
-//           value.
-// (4) The # of distinct int values the IntSet currently contains
-//     is stored in the member variable used.
-// (5) Except when the IntSet is empty (used == 0), ALL elements
-//     of data from data[0] until data[used - 1] contain relevant
-//     distinct int values; i.e., all relevant distinct int values
-//     appear together (no "holes" among them) starting from the
-//     beginning of the data array.
-// (6) We DON'T care what is stored in any of the array elements
-//     from data[used] through data[IntSet::MAX_SIZE - 1].
-//     Note: This applies also when the IntSet is empry (used == 0)
-//           in which case we DON'T care what is stored in any of
-//           the data array elements.
-//     Note: A distinct int value in the IntSet can be any of the
-//           values an int can represent (from the most negative
-//           through 0 to the most positive), so there is no
-//           particular int value that can be used to indicate an
-//           irrelevant value. But there's no need for such an
-//           "indicator value" since all relevant distinct int
-//           values appear together starting from the beginning of
-//           the data array and used (if properly initialized and
-//           maintained) should tell which elements of the data
-//           array are actually relevant.
+/* Daniel Lepe
+   Data Structures 3358
+   02/05/19*/
 
 #include "IntSet.h"
 #include <iostream>
@@ -80,8 +41,14 @@ bool IntSet::contains(int anInt) const
 
 bool IntSet::isSubsetOf(const IntSet& otherIntSet) const
 {
-   cout << "isSubsetOf() is not implemented yet..." << endl;
-   return false; // dummy value returned
+   if(this->isEmpty() && !otherIntSet.isEmpty())
+      return true;
+   for(int i = 0; i < used; ++i)
+   {
+      if(!otherIntSet.contains(data[i]))
+         return false;
+   }
+   return true;
 }
 
 void IntSet::DumpData(ostream& out) const
@@ -96,20 +63,51 @@ void IntSet::DumpData(ostream& out) const
 
 IntSet IntSet::unionWith(const IntSet& otherIntSet) const
 {
-   cout << "unionWith() is not implemented yet..." << endl;
-   return IntSet(); // dummy IntSet object returned
+   try
+   {
+      if((size() + (otherIntSet.subtract(*this)).size()) > MAX_SIZE)
+         throw "sets are too large";
+   }
+   catch(const char* msg)
+   {
+      cerr << msg << endl;
+   }
+   IntSet temp;
+   for(int i = 0; i < used; ++i)
+      temp.add(data[i]);
+   for(int e = 0; e < otherIntSet.size(); ++e)
+   {
+      if(!contains(otherIntSet.data[e]))
+         temp.add(otherIntSet.data[e]);
+   }
+   return temp; 
 }
 
 IntSet IntSet::intersect(const IntSet& otherIntSet) const
 {
-   cout << "intersect() is not implemented yet..." << endl;
-   return IntSet(); // dummy IntSet object returned
+   IntSet temp;
+   for(int i = 0; i < used; ++i)
+   {
+      temp.add(data[i]);
+      if(otherIntSet.contains(data[i]))
+         temp.remove(data[i]);
+   }
+      
+
+   return temp; 
 }
 
 IntSet IntSet::subtract(const IntSet& otherIntSet) const
 {
-   cout << "subtract() is not implemented yet..." << endl;
-   return IntSet(); // dummy IntSet object returned
+   IntSet temp;
+   for(int i = 0; i < used; ++i)
+      temp.add(data[i]);
+   for(int e = 0; e < otherIntSet.size(); ++e)
+   {
+      if(temp.contains(otherIntSet.data[e]))
+         temp.remove(otherIntSet.data[e]);
+   }
+   return temp; 
 }
 
 void IntSet::reset()
@@ -166,9 +164,10 @@ bool equal(const IntSet& is1, const IntSet& is2)
 {
    if(is1.size() == is2.size())
    {
-      if(is1.isEmpty && is2.isEmpty())
+      if(is1.isEmpty() && is2.isEmpty())
          return true;
-      
+      if(is1.isSubsetOf(is2))
+         return true;
    }
    return false; 
 }
